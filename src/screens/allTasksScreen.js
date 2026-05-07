0b6da12d3574fe6c9dcab07c3181f3f78ec31a97
bmv-stack <bmv@narola.email>
2026-05-07 15:40:35 +0530

    Addedlist generation function

--- src/screens/allTasksScreen.js
+++ src/screens/allTasksScreen.js
@@ -25,23 +25,7 @@ import { useTheme } from '../contexts/ThemeContext';
 import { FilterModal, DeleteModal, PriorityModal } from '../components/Modals';
 import { formatDate } from '../utils/formatDate';
 import { formatTime } from '../utils/formatTime';
-
-// -----DateList Generation function-----
-const generateDateList = () => {
-  const dates = [];
-  for (let i = -30; i < 355; i++) {
-    const d = new Date();
-    d.setDate(d.getDate() + i);
-    dates.push({
-      fullDate: d.toISOString().split('T')[0], // ISO String returns YYYY-MM-DDTHH:mm:ss.ss
-      dayNumber: d.getDate().toString(),
-      dayName: d
-        .toLocaleDateString('en-GB', { month: 'short', weekday: 'short' })
-        .toUpperCase(),
-    });
-  }
-  return dates;
-};
+import { generateDateList } from '../utils/dateListGeneration';
 
 const AllTasksScreen = () => {
   // -----THEME-----
---
+++ src/utils/dateListGeneration.js
@@ -0,0 +1,15 @@
+export const generateDateList = () => {
+  const dates = [];
+  for (let i = -30; i < 355; i++) {
+    const d = new Date();
+    d.setDate(d.getDate() + i);
+    dates.push({
+      fullDate: d.toISOString().split('T')[0], // ISO String returns YYYY-MM-DDTHH:mm:ss.ss
+      dayNumber: d.getDate().toString(),
+      dayName: d
+        .toLocaleDateString('en-GB', { month: 'short', weekday: 'short' })
+        .toUpperCase(),
+    });
+  }
+  return dates;
+};
